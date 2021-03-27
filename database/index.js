0dd65b1932bcdda6f525815a9d2ef4024366fdd2
dymafr <contact@dyma.fr>
2021-03-27 14:28:36 +0100

    feat: maj

--- database/index.js
+++ database/index.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true', { 
-  useNewUrlParser: true
-}).then( () => console.log('connexion db ok !')).catch( err => console.log(err));
+exports.clientPromise = mongoose
+  .connect(
+    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .catch((err) => console.log(err));
