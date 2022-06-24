48982a433f1d0ce485374e5688bca12715dec6f2
dymafr <contact@dyma.fr>
2022-06-24 18:23:00 +0200

    maj

--- database/index.js
+++ database/index.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 
 exports.clientPromise = mongoose
   .connect(
-    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }
+    'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true'
   )
   .catch((err) => console.log(err));
